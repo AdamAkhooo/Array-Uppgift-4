489af3a95ac77e02231a869798e9a4c39278f9e8
Meysam03 <simon.munter@icloud.com>
2023-01-27 10:37:28 +0100

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[]args)
+    {
+        string[] namn = { "Walter", "Leon", "Alem", "Suber", "Adam" };
+        Array.Sort(namn);
+        for(int i = 0; i < namn.Length; i++)
+        {
+            Console.WriteLine(namn[i]);
+        }  
+    }
+}
